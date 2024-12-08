30f5a3608a060443736ade96f4f7494a5b87099c
Shermaine Yung <136255659+ycn999@users.noreply.github.com>
2024-12-08 09:38:25 +0800

    Create 20241201.py

---
+++ 20241201.py
@@ -0,0 +1,45 @@
+def pair_transform(pair_text):
+  left = []
+  right = []
+
+  pairs = pair_text.split("\n")
+
+  for p in pairs:
+    left.append(int(p.split(" ")[0]))
+    right.append(int(p.split(" ")[-1]))
+
+  return left, right
+  
+pairs = pair_transform(input)
+
+
+def find_distance(pairs):
+  for lists in pairs:
+    left_sort = sorted(pairs[0])
+    right_sort = sorted(pairs[1])
+
+  rows = len(left_sort) 
+  distance = 0
+
+  for i in range(0, rows):
+    distance += abs(left_sort[i] - right_sort[i])
+
+  return distance
+  
+# puzzle 1 output
+puzzle1 = find_distance(pairs)
+
+def find_similarity(pairs):
+  
+  left_list = pairs[0]
+  right_list = pairs[1]
+  rows = len(left_list)
+
+  score = 0
+  for i in range(0, rows):
+    score += sum(r == left_list[i] for r in right_list) * left_list[i]
+  
+  return score
+
+# puzzle 2 output
+puzzle2 = find_similarity(pairs)
